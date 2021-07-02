815199055f122b4c675d4de8b12c3a36f493d1b5
Janine Amelie Lourens <janineamelie@gmail.com>
2021-07-02 10:51:17 +0200

    setup app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [image, setImage] = useState({ preview: "", raw: "" });
+
+  const handleImageChange = (e) => {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      setImage({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+
+  let {imagePreviewUrl} = image;
+  let imagePreview = null;
+  if (imagePreviewUrl) {
+    imagePreview = (<img alt="maze" src={imagePreviewUrl} />);
+  } else {
+    imagePreview = (<div className="previewText">Please select a maze image to upload</div>);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <div className="previewComponent">
+          <input className="fileInput" 
+            type="file" 
+            onChange={(e)=> handleImageChange(e)} />
+        <div className="imgPreview">
+          {imagePreview}
+        </div>
+      </div>
     </div>
   );
 }
